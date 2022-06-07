89d33b78d85d85f844aca5b9d450779039a8d66f
Jennis Florez <usuario@DESKTOP-FS19265>
2022-06-07 10:07:55 -0500

    Fix scripts to update sync full templates folder

--- 02.aws_copy_resources.sh
+++ 02.aws_copy_resources.sh
@@ -7,6 +7,6 @@ echo "Copying local resources to S3 buckets with AWS CLI"
 
 aws s3 sync aws_IaC s3://$files_s3_bucket/IaC
 aws s3 cp lambda_code/target/$lambda_packaged_file s3://$files_s3_bucket/lambda/
-aws s3 cp lambda_test/template.jrxml s3://$templates_s3_bucket
+aws s3 sync lambda_test s3://$templates_s3_bucket
 
 echo "Local resources copied"
